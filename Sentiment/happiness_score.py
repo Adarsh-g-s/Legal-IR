b58c0786d893dd2d5e7dc2e90610da8cc4486d2c
ranjithshivakumaraswamy <34473518+ranjithshivakumaraswamy@users.noreply.github.com>
2017-12-25 11:47:23 +0100

    happiness score file for sentiment analysis added

---
+++ Sentiment/happiness_score.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url=open("E:\\Masters\\3rd semester\\DKE project\\document new\\Abortion\\108713.json.html")#108713.json.html
+soup=BeautifulSoup(url,"lxml")
+entire_text=soup.get_text()
+normalized_Frequency=0.0  
+total_happiness_score=0.0
+#code to get list of word and its happiness score from document
+d = {}
+with open("data1.txt") as i:
+    for line in i:
+       (key, val) = line.split()
+       d[key] = val
+
+
+#code to get each word count present in document
+def count_all_words(entire_text):
+    count_words = dict()
+    words = entire_text.split()
+
+    for word in words:
+        if word in count_words:
+            count_words[word] += 1
+        else:
+            count_words[word] = 1
+
+    return count_words
+
+
+count_of_words=count_all_words(entire_text) # store all the word counts in a
+
+#code for getting normalized frequency
+for key in count_of_words :
+    normalized_Frequency=normalized_Frequency+(count_of_words[key]) #all frequency of each word in document to get normalised frequency
+   # print(a[key])
+#print(normalizedFrequency)
+
+
+
+for key in count_of_words :
+    if key in d :
+        countall=countall+1
+        x=d[key] #happiness avg score of word
+        y=count_of_words[key] #frequency of word
+        total_happiness_score=total_happiness_score+float(x)*(float(y)/normalized_Frequency)
+
+print(total_happiness_score)
