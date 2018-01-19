160257115ed16403cc4e5c16c8c0d867d92b3f99
ranjithshivakumaraswamy <34473518+ranjithshivakumaraswamy@users.noreply.github.com>
2018-01-19 15:19:59 +0100

    updated svm classifier

--- Sentiment/svmclassifier.py
+++ Sentiment/svmclassifier.py
@@ -1,12 +1,12 @@
+import numpy as np
 import pandas as pd 
-load=pd.read_csv('train.csv',delimiter=',',skipinitialspace=True)
-feature_cols_train=['nltksscore','happyscore','positive','negative']
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+load=pd.read_csv('res.csv',delimiter=',',skipinitialspace=True)
+feature_cols_train=['negative_word_count','positive_word_count','vander_score','happ_score']
 X=load[feature_cols_train]
-y=load.sentiment
-loadtest=pd.read_csv('test.csv',delimiter=',',skipinitialspace=True)
-feature_cols_test=['nltksscore','happyscore','positive','negative']
-from sklearn.svm import SVC
-clf = SVC()
-clf.fit(X, y) 
-predictions=clf.predict(loadtest[feature_cols_test])
-pd.DataFrame(predictions, columns=['predictions']).to_csv('prediction.csv')
+y=load.Sentiment
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+clf.score(X_test, y_test) 
