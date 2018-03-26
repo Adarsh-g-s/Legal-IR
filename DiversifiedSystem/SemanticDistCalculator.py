1ff606a82fbec154dbac237f1c11846fcaab2079
Rohith Ravindran <rohithravi@gmail.com>
2018-03-26 16:17:34 +0200

    Using HashingVectorizer instead of TfidfVectorizer

--- DiversifiedSystem/SemanticDistCalculator.py
+++ DiversifiedSystem/SemanticDistCalculator.py
@@ -1,19 +1,21 @@
 from sklearn.metrics.pairwise import cosine_distances
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
 
 class SemanticDistCalculator(object):
     """description of class"""
     def learnVocabulary(self, filePaths):
         self.filePaths = filePaths
-        self.tfidf_vectorizer = TfidfVectorizer(input='filename', stop_words='english',
-                                                max_features=500, max_df=0.8, min_df=0.2)
-        self.tfidf_vectorizer.fit(self.filePaths)
-        # print(self.tfidf_vectorizer.vocabulary_)
+        #self.vectorizer = TfidfVectorizer(input='filename', stop_words='english',
+        #                                        max_features=500, max_df=0.8, min_df=0.2)
+        self.vectorizer = HashingVectorizer(input='filename', stop_words='english')
+        # self.vectorizer.fit(self.filePaths)
+        # print(self.vectorizer.vocabulary_)
 
     def showDocDistMatrix(self):
-        tfidf_matrix = self.tfidf_vectorizer.transform(self.filePaths)
-        # print(tfidf_matrix)
-        self.tfidf_cosine_distance_array = cosine_distances(tfidf_matrix)
+        transformed_matrix = self.vectorizer.transform(self.filePaths)
+        # print(transformed_matrix)
+        self.tfidf_cosine_distance_array = cosine_distances(transformed_matrix)
         print(self.tfidf_cosine_distance_array)
 
     # def getDistances(self, filepaths):
@@ -22,8 +24,8 @@ class SemanticDistCalculator(object):
         # tempDocList.append(filepaths[0])
         # for docIndex in range(1, len(filepaths)):
             # tempDocList.append(filepaths[docIndex])
-            # tfidf_matrix = self.tfidf_vectorizer.transform(tempDocList)
-            # cosine_dist_array = cosine_distances(tfidf_matrix)
+            # transformed_matrix = self.vectorizer.transform(tempDocList)
+            # cosine_dist_array = cosine_distances(transformed_matrix)
             # # print(cosine_dist_array)
             # # TODO:
             # distList.append(cosine_dist_array[0][1])
@@ -32,8 +34,9 @@ class SemanticDistCalculator(object):
         # return distList
 
     def calcDistMatrix(self):
-        tfidf_matrix = self.tfidf_vectorizer.transform(self.filePaths)
-        self.cosine_dist_array = cosine_distances(tfidf_matrix)
+        transformed_matrix = self.vectorizer.transform(self.filePaths)
+        self.cosine_dist_array = cosine_distances(transformed_matrix)
+        print(self.cosine_dist_array[0])
         
     def getDistances(self, fileIndex):
         distArray = self.cosine_dist_array[fileIndex]
